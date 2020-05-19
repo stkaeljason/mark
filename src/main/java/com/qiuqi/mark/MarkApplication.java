9c75a50e09e70dbf688128b879873caa67594bdd
jqy <pudge702@qq.com>
2020-05-19 16:26:19 +0800

    create mark project

---
+++ src/main/java/com/qiuqi/mark/MarkApplication.java
@@ -0,0 +1,13 @@
+package com.qiuqi.mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkApplication.class, args);
+	}
+
+}
